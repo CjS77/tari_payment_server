1314571346d164d25255a8f1a6370ba584986137
Cayle Sharrock <CjS77@users.noreply.github.com>
2024-06-27 14:49:24 +0100

    feat: preflight checks (#10)
    Adds a safety feature that will prevent the TPG server from running if
    certain critical environmental variables or other config has not been
    setup correctly.

--- tari_payment_server/src/main.rs
+++ tari_payment_server/src/main.rs
@@ -1,6 +1,13 @@
+use std::{env, str::FromStr};
+
 use dotenvy::dotenv;
-use log::info;
-use tari_payment_server::{cli::handle_command_line_args, config::ServerConfig, server::run_server};
+use log::{error, info};
+use tari_common_types::tari_address::TariAddress;
+use tari_payment_server::{
+    cli::handle_command_line_args,
+    config::{AuthConfig, ServerConfig},
+    server::run_server,
+};
 
 #[actix_web::main]
 async fn main() {
@@ -8,10 +15,61 @@ async fn main() {
     env_logger::init();
     handle_command_line_args();
     let config = ServerConfig::from_env_or_default();
+    if !preflight_check(&config) {
+        eprintln!("🚀️ Preflight check failed. Exiting. Check the logs for details.");
+        return;
+    }
 
     info!("🚀️ Starting server on {}:{}", config.host, config.port);
     match run_server(config).await {
         Ok(_) => println!("Bye!"),
         Err(e) => eprintln!("{e}"),
     }
+}
+
+fn preflight_check(config: &ServerConfig) -> bool {
+    if env::var("TPG_SKIP_PREFLIGHT").ok() == Some("Yes".to_string()) {
+        info!("🚦️ Skipping preflight checks. I hope you know what you're doing!");
+        return true;
+    }
+    let mut result = true;
+    info!("🚦️ Running preflight checks...");
+    info!("🚦️ Checking for required environment variables...");
+    if AuthConfig::try_from_env().is_err() {
+        error!("🚦️ Preflight check FAILED: You must set up the JWT signing keys before carrying on.");
+        result = false;
+    }
+    result = match env::var("TPG_PAYMENT_WALLET").ok() {
+        Some(addr) => {
+            let valid = TariAddress::from_str(&addr).is_ok();
+            if !valid {
+                error!(
+                    "🚦️ TPG_PAYMENT_WALLET is not a valid Tari address. Please set it to the address that customers \
+                     send funds to."
+                );
+            }
+            valid
+        },
+        None => {
+            error!(
+                "🚦️ TPG_PAYMENT_WALLET is not set. This needs to be configured to the address that customers send \
+                 funds to. If you don't set it, funds will be donated to the developers."
+            );
+            false
+        },
+    };
+    if config.database_url.is_empty() {
+        error!("🚦️ TPG_DATABASE_URL is not set. Please set it to the URL for the TPG database.");
+        return false;
+    }
+    if !result {
+        error!("🚦️ Preflight check FAILED: Please fix the issues above before starting the server.");
+        info!(
+            "🚦️ If you really know what you're doing and want to skip the preflight check, set `TPG_SKIP_PREFLIGHT` \
+             to `Yes` in your environment variables"
+        );
+    } else {
+        info!("🚦️ Preflight check PASSED.");
+    }
+    result
 }
