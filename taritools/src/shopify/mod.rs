aea3393d8f574100d735d8b47d4f8f7ae0173786
Cayle Sharrock <CjS77@users.noreply.github.com>
2024-10-15 17:59:46 +0100

    feat: accept arbitrary numbers as order ids
    If SHOPIFY_ORDER_FIELD is name, the following payment id strings will
    match to order ids:
    
    "12345" -> #12345
    "#12345" -> #12345
    "#00001" -> #00001
    Order#12345  -> #12345
    Order# 12345  -> #12345
    order#12345  -> #12345
    order 12345 -> #12345
    Order ID is 1234 -> #1234
    Track Order 9876. This is 500 ->  -> #9876
    order 123 456 ->  -> #123
    123 45 67 -> #123
    "#5674 Main st" -> #5674
    5674 Main st -> #5674
    "#5674. Phone 555-666-1111" -> #5674
    
    These payment ids will not match anything
    555-888-9995
    phone: 555-888-9995
    Order#

--- taritools/src/shopify/mod.rs
+++ taritools/src/shopify/mod.rs
@@ -1,5 +1,7 @@
 mod command_def;
 mod command_handler;
+mod config;
 
 pub use command_def::{OrdersCommand, ShopifyCommand};
 pub use command_handler::handle_shopify_command;
+pub use config::order_id_field_from_env;
