9f6a45eb8a7e81c54511f1037ceeeedfbf0778e4
Cayle Sharrock <CjS77@users.noreply.github.com>
2024-11-01 08:53:28 +0000

    tests: multi-threaded burst orders
    To test write contention on the sqlite database, the burst orders test
    is rewritten to hit the database from multiple threads.

--- tari_payment_engine/tests/burst_orders.rs
+++ tari_payment_engine/tests/burst_orders.rs
@@ -1,5 +1,12 @@
-use std::time::Duration;
+use std::{
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
+use futures_util::future::join_all;
 use log::*;
 use tari_payment_engine::{
     db_types::*,
@@ -8,39 +15,58 @@ use tari_payment_engine::{
     OrderFlowApi,
     SqliteDatabase,
 };
-use tokio::runtime::Runtime;
+use tokio::runtime::{Builder, Runtime};
 use tpg_common::MicroTari;
 
-const NUM_ORDERS: u64 = 20;
+const NUM_BATCHES: usize = 20;
+const NUM_THREADS: usize = 5;
 const RATE: u64 = 100; // orders per second
 
 #[test]
 fn burst_orders() {
     info!("🚀️ Starting order injection test");
 
-    let sys = Runtime::new().unwrap();
+    let sys = Builder::new_multi_thread().worker_threads(NUM_THREADS).enable_time().build().unwrap();
 
-    let delay = Duration::from_millis(1000 / RATE);
+    let delay = Duration::from_millis(1000 / RATE * 5);
 
+    let successes = Arc::new(AtomicU64::new(0));
+    let s2 = successes.clone();
+    let num_orders = NUM_BATCHES * NUM_THREADS;
     sys.block_on(async move {
         let url = "sqlite://../data/test_burst_orders.db";
         prepare_test_env(url).await;
-        let db = SqliteDatabase::new_with_url(url, 5).await.expect("Error creating database");
-        let api = OrderFlowApi::new(db, EventProducers::default());
+        let db = SqliteDatabase::new_with_url(url, NUM_THREADS as u32).await.expect("Error creating database");
 
-        let mut timer = tokio::time::interval(delay);
-        info!("🚀️ Injecting {NUM_ORDERS} orders");
-        for i in 0..NUM_ORDERS {
-            timer.tick().await;
-            let cid = ((i + 1) % 5).to_string();
-            #[allow(clippy::cast_possible_wrap)]
-            let price = MicroTari::from(1_000_000 * (i + 1) as i64);
-            let order_id = OrderId::from(format!("oid-2024/{}-burstorder", i * 100));
-            let new_order = NewOrder::new(order_id, cid, price);
-            if let Err(e) = api.process_new_order(new_order, true, true).await {
-                panic!("Error processing order {i}: {e}");
-            }
+        info!("🚀️ Injecting {num_orders} orders");
+        let mut tasks = Vec::with_capacity(NUM_BATCHES);
+        for t in 0..NUM_THREADS {
+            let db2 = db.clone();
+            let s3 = s2.clone();
+            let task = tokio::spawn(async move {
+                let api = OrderFlowApi::new(db2, EventProducers::default());
+
+                for i in 0..NUM_BATCHES {
+                    let mut timer = tokio::time::interval(delay);
+                    timer.tick().await;
+                    let cid = ((i + 1) % 5).to_string();
+                    #[allow(clippy::cast_possible_wrap)]
+                    let price = MicroTari::from(1_000_000 * (i + 1) as i64);
+                    let order_id = OrderId::from(format!("oid-2024/{}.{}-burstorder", t, i * 100));
+                    let new_order = NewOrder::new(order_id, cid, price);
+                    if let Err(e) = api.process_new_order(new_order, true, true).await {
+                        panic!("Error processing order {i}: {e}");
+                    } else {
+                        s3.fetch_add(1, Ordering::Relaxed);
+                    }
+                }
+            });
+            tasks.push(task);
         }
+        let results = join_all(tasks).await;
+        assert!(results.iter().all(|r| r.is_ok()), "Not all threads completed happily");
     });
+    let successes = successes.as_ref().load(Ordering::SeqCst);
+    assert_eq!(successes, num_orders as u64);
     info!("🚀️ test complete");
 }
